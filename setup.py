2414ba2d3491fb89f572f7060b4516202935a58d
CSLedPI <cpsc-admin@calvin.edu>
2024-11-12 16:03:50 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="calvin-cs-matrix-library",
     packages=['matrix_library'],
-    ]),
     version="0.1.0",
     python_requires=">=3.9",
     description="A Python library for use of Computer Science Department at Calvin University's matrix LED display",
@@ -12,5 +11,5 @@ setup(
     url='https://github.com/LEDwallSeniorProject/library',
     install_requires=[
         'numpy','matplotlib','PIL','pygame','scikit-image'
-    ]
+    ],
 )
