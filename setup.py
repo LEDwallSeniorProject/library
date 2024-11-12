95722642adb77e8fe976598cce239cce8100ceff
CSLedPI <cpsc-admin@calvin.edu>
2024-11-12 15:59:05 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="Matrix Library",
-    packages=find_packages(),
+    name="calvin-cs-matrix-library",
+    packages=['matrix_library'],
+    ]),
     version="0.1.0",
-    description="A Python library for use of Computer Science Department at Calvin University's matrix display",
+    python_requires=">=3.9",
+    description="A Python library for use of Computer Science Department at Calvin University's matrix LED display",
     author="Alex Miller, Ellie Sand, Palmer Ford, and Professor Chris Wieringa",
+    author_email='cpsc-admin@calvin.edu',
+    url='https://github.com/LEDwallSeniorProject/library',
+    install_requires=[
+        'numpy','matplotlib','PIL','pygame','scikit-image'
+    ]
 )
