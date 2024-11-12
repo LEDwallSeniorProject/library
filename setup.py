d2f73a263457e3a2d4232c6e379242916463f321
Chris Wieringa <cwieri39@calvin.edu>
2024-11-12 16:35:34 -0500

    updated python versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="calvin-cs-matrix-library",
     packages=['matrix_library'],
     version="0.1.0",
-    python_requires=">=3.9",
+    python_requires=">=3.8,<3.12",
     description="A Python library for use of Computer Science Department at Calvin University's matrix LED display",
     author="Alex Miller, Ellie Sand, Palmer Ford, and Professor Chris Wieringa",
     author_email='cpsc-admin@calvin.edu',
