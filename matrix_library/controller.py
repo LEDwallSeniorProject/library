29e3e9c0d91b160010b2be1a7931cdfb8655effa
Chris Wieringa <chris@wieringafamily.com>
2024-12-02 11:08:15 -0500

    initial evdev async support

--- matrix_library/controller.py
+++ matrix_library/controller.py
@@ -14,36 +14,35 @@
 import time
 import re
 import platform
-from threading import Thread
+import logging
 
 # TODO: change this to make it like the others
 # Detection of Platform for import
 if re.search("armv|aarch64", platform.machine()) and re.search(
     "csledpi", platform.node()
 ):
-    from evdev import InputDevice, categorize, ecodes, list_devices
+    import asyncio, evdev
     mode = "board"
 
 else:
-    import pygame
-    import keyboard
-    mode = "debug"
-
+    import keyboard, pygame
+    mode = "workstation"
 
 class Controller:
     def __init__(self):
         self.function_map = {}
-        self.threads = []
+        self.debug = True
 
+        # setup the LEDwall with evdev for controller inputs
         if mode == "board":
             while self.gamepad is None:
                 try:
                     # autodetection of gamepad
                     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
                     if(len(devices) > 0):
-                        self.gamepad = InputDevice(devices[0].path)
+                        self.gamepad = evdev.InputDevice(devices[0].path)
                 except:
-                    print("No gamepad found")
+                    logging.debug("No gamepad found") if self.debug
                     time.sleep(1)
             self.button_map = {
                 "LB": 37,
@@ -57,7 +56,14 @@ class Controller:
                 "Y": 23,
                 "X": 35,
             }
-        else:
+
+            # setup evdev async/await functions
+            asyncio.ensure_future(self._gamepad_event(self.gamepad))
+            self.loop = asyncio.get_event_loop()
+            self.loop.run_forever()
+
+        # setup workstation mode with pygame and keyboard input
+        elif mode == "workstation":
             self.button_map = {
                 "LB": "q",
                 "RB": "e",
@@ -71,24 +77,31 @@ class Controller:
                 "X": "i",
             }
 
+    # asyncio gamepad_event function -- internal use only
+    async def _gamepad_events(self,device):
+        async for event in device.async_read_loop():
+            logging.debug(f"_gamepad_events", device.path, evdev.categorize(event), sep=': ') if self.debug
+
+            if event.type == evdev.ecodes.EV_KEY:
+                    key_event = evdev.categorize(event)
+                    if (key_event.keystate == key_event.key_down or 
+                        key_event.keystate == key_event.key_hold):
+                            button = list(self.button_map.keys())[list(self.button_map.values()).index(key_event.keycode)]
+
+                            logging.debug(f"_gamepad_events: button {button} function {self.function_map[button]["functionName"]}") if self.debug
+                            self.function_map[button]["function"]()
+
     def add_function(self, button, function):
         if mode == "board":
-            t = Thread(target=self.worker, args=(button, function), daemon=True)
-            t.start()
-            print(f"Thread started: {button}")
-            self.threads.append(t)
-        else:
-            print(f"Key: {self.button_map[button]}")
+            self.function_map[button] = {
+                "functionName":  function,
+                "function": globals().get(function),
+                "button": button,
+            }
+
+        elif mode == "workstation":
+            logging.debug(f"Key: {self.button_map[button]}") if self.debug
             keyboard.add_hotkey(self.button_map[button], function)
 
-    def worker(self, button, function):
-        while True:
-            for event in self.gamepad.read_loop():
-                if event.type == ecodes.EV_KEY:
-                    key_event = categorize(event)
-                    if (
-                        key_event.keycode == self.button_map[button]
-                        and key_event.keystate == key_event.key_down
-                    ):
-                        print("running function")
-                        function()
+        else:
+            logging.warning(f"Unhandled mode: {mode}, button: {button}, function: {function}")
