ff68fd8d3939fb761d8c9c370764c21e07975d6d
CSLedPI <cpsc-admin@calvin.edu>
2024-12-10 09:25:51 -0500

    updated random_image loading

--- examples/show-random-internet-pic.py
+++ examples/show-random-internet-pic.py
@@ -1,34 +1,60 @@
-# Imports
 from PIL import Image
+import matrix_library as matrix
 import time
 import requests
 import io
 import sys
-from matrix_library import canvas as c, shapes as s
+
+# program setup
+controller = matrix.Controller()
+canvas = matrix.Canvas()
+
+# variables
+exited = False
+loading = matrix.Phrase(text="Loading image...",position=[0,120],size=1)
 
 def exit_prog():
-    print(quit)
-    # canvas.delete()
-    sys.exit()
+    global exited
+    exited = True
 
 # random picture URL
 w = h = 128
 url = f"https://picsum.photos/{w}"
 
-#canvas = c.Canvas(renderMode="zmq",zmqRenderTarget="localhost",zmqRenderPort=5500)
-canvas = c.Canvas()
-canvas.clear()
+controller.add_function("START", exit_prog)
 
-r = requests.get(url, stream=True)
-if r.status_code == 200:
-    img = Image.open(io.BytesIO(r.content))
-    pixels = list(img.getdata())
-    #print(pixels)
+for i in range(0,3):
+    if not exited:
+        
+        canvas.add(loading)
+        canvas.draw()
 
-    img = s.Image(width=128,height=128,position=[0,0])
-    img.loadpixels(pixels)
+        r = requests.get(url, stream=True, timeout=2)
+        if r.status_code == 200:
+            img = Image.open(io.BytesIO(r.content))
+            pixels = list(img.getdata())
+            #print(pixels)
+            img = matrix.Image(width=128,height=128,position=[0,0])
+            img.loadpixels(pixels)
 
-    canvas.add(img)
-    canvas.draw()
+            canvas.add(img)
+            canvas.draw()
+        else:
+            phrase = matrix.Phrase("Image load failed")
+            canvas.add(phrase)
+            canvas.draw()
+        
+        # pause for a second between them, but check for exits
+        maxwait = 3
+        timer = 0
+        sleepstep = 0.25
+        while timer < maxwait:
+            if exited: timer += 10000
+            else: 
+                time.sleep(sleepstep)
+                timer += sleepstep
+    
 
-    time.sleep(10)
+# clear the screen before exiting
+print("quit")
+sys.exit(0)
