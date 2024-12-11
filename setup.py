e8f20a9be9066fed470016b36f37750914964eb5
Chris Wieringa <chriswier@users.noreply.github.com>
2024-12-11 10:58:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages, setup
 setup(
     name="calvin-cs-matrix-library",
     packages=['matrix_library'],
-    version="0.1.0",
+    version="1.0.0",
     python_requires=">=3.8,<3.12",
     description="A Python library for use of Computer Science Department at Calvin University's matrix LED display",
     author="Alex Miller, Ellie Sand, Palmer Ford, and Professor Chris Wieringa",
     author_email='cpsc-admin@calvin.edu',
     url='https://github.com/LEDwallSeniorProject/library',
     install_requires=[
-        'numpy','matplotlib','pillow','pygame','scikit-image','keyboard'
+        'numpy','matplotlib','pillow','pygame','scikit-image','keyboard','requests'
     ],
     setup_requires=['wheel'],
 )
