a2f06ead587d0e884fc7b79567cfe9d37d79a659
Eli Lewis <elilewis327@gmail.com>
2025-04-07 13:16:54 -0400

    changed looping mechanism

--- matrix_library/LEDWall.py
+++ matrix_library/LEDWall.py
@@ -1,7 +1,8 @@
 import time
+from matrix_library import shapes
 
 class LEDProgram:
-    def __init__(self, canvas, controller, fps=60):
+    def __init__(self, canvas, controller, fps=60, trackFPS=False):
         self.canvas = canvas
         self.controller = controller
         # check that things are defined correctly
@@ -40,6 +41,7 @@ class LEDProgram:
         self.__bind_controls__()
         self.running = True
         self.__exited__ = False
+        self.trackFPS = trackFPS
 
         self.__fps__ = fps
 
@@ -52,28 +54,52 @@ class LEDProgram:
 
     def __loop__(self):
         self.preLoop()
-        last_time = time.time()
-        frame_time = 1 / self.__fps__
-        frames = 0
+        last = time.time()
+        max_fps = self.__fps__
+        max_frame_time = 1 / max_fps
+        expected_frame_time = max_frame_time
+        start = last
+        fps_count = 0
+        fps_time = 0
+        avg_fps = 0
 
         while self.running:
-            current_time = time.time()
-            elapsed_time = current_time - last_time
-
-            while elapsed_time < frame_time:
-                current_time = time.time()
-                elapsed_time = current_time - last_time
-                time.sleep(.001)
-
-            last_time = current_time
-            frames += 1
-
+            start_time = time.time()
+            
             self.canvas.clear()
             self.__draw__()
+            if self.trackFPS:
+                fps_phrase = shapes.Phrase(f"FPS: {avg_fps:.0f}", [0,0])
+                self.canvas.add(fps_phrase)
             self.canvas.draw()
 
+            elapsed = time.time() - start_time
+            sleep_time = expected_frame_time - elapsed
+
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+            
+            now = time.time()
+            dif = now - last
+            last = now
+
+            fps_time += dif
+            fps_count += 1
+
+            if fps_time >= 1:
+                avg_fps = fps_count
+                fps_count = 0
+                fps_time = 0
+
+                if avg_fps > max_fps:
+                    expected_frame_time += max_frame_time * 0.05
+                elif (max_fps - avg_fps) > 3:
+                    expected_frame_time -= max_frame_time * 0.01
+                
         self.postLoop()
         self.__unbind_controls__()
+
+       
 
     def __unbind_controls__(self):
         self.controller.clear()
